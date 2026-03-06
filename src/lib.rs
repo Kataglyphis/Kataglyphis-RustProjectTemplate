eb8beb044848b4a182bfe877a97f14c4b2477e50
Jonas Heinle <jonas.heinle@ipa.fraunhofer.de>
2026-03-06 19:04:26 +0100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 #![doc = include_str!("../docs/_static/getting-started.md")]
 
+#[cxx::bridge]
+mod ffi {
+	extern "Rust" {
+		fn rusty_cxxbridge_integer() -> i32;
+	}
+}
+
 pub mod api;
 
 #[cfg(feature = "burn_demos")]
@@ -8,6 +15,10 @@ pub mod burn_demos;
 mod frb_generated;
 #[cfg(any(feature = "onnx_tract", feature = "onnxruntime"))]
 mod person_detection;
+
+pub fn rusty_cxxbridge_integer() -> i32 {
+	322
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn rusty_extern_c_integer() -> i32 {
